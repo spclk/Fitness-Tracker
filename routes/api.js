8e57e03884c45669b194c525563972410d88661c
Konstantin <konstantin1020@gmail.com>
2021-04-14 22:22:56 -0700

    added model files and api folder in routes

---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const Model = require("../models/model.js");
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
