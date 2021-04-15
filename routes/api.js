ac03c6cfd68a5f30419d66ba20f2c56747309f59
Konstantin <konstantin1020@gmail.com>
2021-04-14 23:00:59 -0700

    added basic routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Fitness = require("../models/fitness.js");
-
+const Fitness = require("../models/Fitness.js");
+const Chart = require("../models/Chart.js");
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
+const db = require("./models");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
